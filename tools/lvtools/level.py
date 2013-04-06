652dca685958650140dff6ddeaf694f7360d3a98
Yuri K. Schlesner <yuriks@yuriks.net>
2013-04-06 09:10:15 -0500

    Level header parsing tool (useful for finding chunks!)

--- tools/lvtools/level.py
+++ tools/lvtools/level.py
@@ -63,32 +63,64 @@ def load_chunk_list(data, pos):
     return pos, chunks
 
 class LoadList(object):
-    # Offset of the load list into the level header
-    LOAD_LIST_OFFSET = 0x43
-
     def __init__(self):
         self.palettes = None
         self.unknown = None
         self.objects = None
         self.animations = None
 
-    def load(self, header_data):
-        load_list = header_data[self.LOAD_LIST_OFFSET:]
-
+    def load(self, load_list):
         current_pos = seek_list(load_list)
         current_pos, self.palettes = load_palette_list(load_list, current_pos)
         current_pos, self.unknown = process_list(load_list, current_pos)
         current_pos, self.objects = load_chunk_list(load_list, current_pos)
         current_pos, self.animations = load_chunk_list(load_list, current_pos)
 
-    def print_list(self):
+    def __str__(self):
+        s = ''
+
         for chunk_id, load_position in self.palettes:
-            print "Load palette chunk 0x%03X into position %d (offs: 0x%03X)" % (
+            s += "Load palette chunk 0x%03X into position %d (offs: 0x%03X)\n" % (
                     chunk_id, load_position, load_position*3)
-        print "Unknown bitset: %02x" % (self.unknown[0],)
+        s += "Unknown bitset: %02x\n" % (self.unknown[0],)
         for vals in self.unknown[1]:
-            print "Unknown data: %02x %02x %02x" % vals
+            s += "Unknown data: %02x %02x %02x\n" % vals
         for chunk_id in self.objects:
-            print "Load object graphics from chunk 0x%03X" % (chunk_id,)
+            s += "Load object graphics from chunk 0x%03X\n" % (chunk_id,)
         for chunk_id in self.animations:
-            print "Load animation graphics from chunk 0x%03X" % (chunk_id,)
+            s += "Load animation graphics from chunk 0x%03X\n" % (chunk_id,)
+
+        return s
+
+class LevelHeader(object):
+    def __init__(self):
+        pass
+
+    def load(self, data):
+        self.next_level = read_word(data, 0x16)
+        self.flags = data[0x1C]
+
+        self.width = read_word(data, 0x29)
+        self.height = read_word(data, 0x29)
+
+        self.tilemap_chunk = read_word(data, 0x2E)
+        self.tileset_chunk = read_word(data, 0x30)
+        self.metatile_chunk = read_word(data, 0x32)
+
+        self.load_list = LoadList()
+        self.load_list.load(data[0x43:])
+
+    format_string = \
+"""Next level: 0x{0.next_level:02X}
+Flags: 0x{0.flags:02X}
+Width (metatiles): {0.width}
+Height (metatiles): {0.height}
+Tilemap chunk: 0x{0.tilemap_chunk:03X}
+Tileset chunk: 0x{0.tileset_chunk:03X}
+Metatile chunk: 0x{0.metatile_chunk:03X}
+
+============ Load list ============
+{0.load_list}"""
+
+    def __str__(self):
+        return self.format_string.format(self)
--- tools/parse_level_header.py
+++ tools/parse_level_header.py
@@ -3,14 +3,10 @@ import sys
 import lvtools.chunk as chunk
 from lvtools.compression import decompress_data
 from lvtools.level import LevelHeader
-import lvtools.palette as palette
-import lvtools.util as util
 
 level_header_id = int(sys.argv[1], 0)
-output_file = sys.argv[2]
 
 header_data = decompress_data(chunk.read_chunk(level_header_id))
 header = LevelHeader()
 header.load(header_data)
-pal = palette.assemble_palette(header.load_list.palettes)
-util.dump_file(output_file, palette.pack_palette(pal))
+print header
