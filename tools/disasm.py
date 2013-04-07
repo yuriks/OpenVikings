e5e1fd95668ee7600a9cf22168f0c54452e18764
Yuri K. Schlesner <yuriks@yuriks.net>
2013-04-06 22:28:30 -0500

    More disasm opcodes

--- tools/disasm.py
+++ tools/disasm.py
@@ -56,28 +56,44 @@ class Op(object):
         self.flow = flow
         self.desc = desc
         self.instr_len = 1 + calc_operands_len(operands)
-        self.is_jmp = '$' in operands
+        self.is_jmp = '$' in operands or flow == self.FLOW_NORETURN
 
 instruction_table = {
     'suffix': '',
     0x00: Op('YIELD', '', desc="save IP and yield"),
     0x01: Op('NOP', ''),
+    0x02: Op('AUDIO.UNK02', '#w', desc="does something with audio"),
     0x03: Op('JMP', '$w', flow=Op.FLOW_NORETURN, desc="unconditional JuMP { goto op0; }"),
     0x05: Op('CALL', '$w', desc="Save next IP to link reg and jump"),
     0x06: Op('RET', '', flow=Op.FLOW_NORETURN, desc="Return to IP saved on link register"),
     0x0F: Op('FINISH.LEVEL', '', flow=Op.FLOW_NORETURN, desc="yield & finish level"),
     0x19: Op('OBJ.UNK19', '#w'),
+    0x1A: Op('J?.OBJ.UNK1A', '#b$w'),
     0x2F: Op('SUBVM2', '', desc="Invoke VM2"),
+    # Variable Length Instruction, needs more work to decode: 0x41: Op('DIALOG.TEXT', '#b__#b'
+    0x46: Op('DIALOG.COLOR', '#w', desc="Appends a dialog color change command"),
     0x51: Op('LDA', '#w', desc="LoaD A { A = op0; }"),
     0x52: Op('OBJPROP.LDA', '#b', desc="LoaD A from current object op0/2"),
+    0x53: Op('LDA', '*w', desc="LoaD A from memory"),
     0x56: Op('OBJPROP.STA', '#b', desc="STore A to current object op0/2"),
+    0x57: Op('STA', '*w', desc="STore A to memory"),
+    0x5A: Op('ADD', '*w', desc="ADD A to memory { *op0 += A; }"),
+    0x5F: Op('OBJPROP.ANDA', '#b', desc="AND current object op0/2 with A"),
+    0x60: Op('ANDA', '*w', desc="AND memory with A { op0 &= A; }"),
     0x62: Op('OBJPROP.ORA', '#b', desc="OR current object property op0/2 with A"),
+    0x68: Op('JB', '#w$w', desc="Jump if Below { if (op0 < A) goto op1; }"),
+    0x72: Op('JE', '#w$w', desc="Jump if Equal { if (op0 == A) goto op1; }"),
     0x73: Op('OBJPROP.JE', '#b$w', desc="jump if current object property op0/2 == A"),
+    0x74: Op('JE', '*w$w', desc="Jump if Equal { if (*op0 == A) goto op1; }"),
+    0x77: Op('JNE', '#w$w', desc="Jump if Not Equal { if (op0 != A) goto op1; }"),
     0x78: Op('OBJPROP.JNE', '#b$w', desc="jump if current object property op0/2 != A"),
+    0x79: Op('JNE', '*w$w', desc="Jump if Not Equal { if (*op0 != A) goto op1; }"),
+    0x96: Op('OBJ.STA.UNK96', '', desc="Store A to unknown object property"),
     0x97: Op('TBS', '#b#w', desc="Test Bit & Set { A = bool((1 << op0/2) & op1) }"),
     0x99: Op('TBS', '#b*w', desc="Test Bit & Set { A = bool((1 << op0/2) & op1) }"),
     0xA8: Op('TBJE', '#b#w$w', desc="Test Bit & Jump if Equal { if (bool((1 << op0/2) & op1) == A) goto op3; }"),
     0xAA: Op('TBJ', '#b*w$w', desc="Test Bit & Jump { if ((1 << op0/2) & op1) goto op3; }"),
+    0xCF: Op('UNKCF', '$w'),
 }
 
 def decode(table, rom, ip, ram_symbols):
@@ -104,7 +120,7 @@ def decode(table, rom, ip, ram_symbols):
 
         if optype == '#': # Immediate
             opr_text.append('#%X' % (value,))
-        elif optype == '*': # Indirect
+        elif optype == '*': # Memory pointer
             opr_text.append('($%s)' % (try_get_symbol(ram_symbols, value),))
         elif optype == '$': # Absolute label
             opr_text.append('$%X' % (value,))
